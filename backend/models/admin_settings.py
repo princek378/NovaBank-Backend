512e9f8685bb6e025ae04d1622faf76d42ba9fc7
princek378 <kokoprince334@gmail.com>
2026-08-06 01:17:18 +0100

    Update admin_settings.py

--- backend/models/admin_settings.py
+++ backend/models/admin_settings.py
@@ -3,38 +3,16 @@ from datetime import datetime
 
 
 class AdminSettings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    admin_name = db.Column(db.String(100), default="Administrator")
+    email = db.Column(db.String(120), default="admin@novabank.com")
+    bank_name = db.Column(db.String(100), default="NovaBank")
+    currency = db.Column(db.String(10), default="USD")
 
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
+    # Transfer security codes
+    imf_code = db.Column(db.String(50), default="IMF-0000")
+    cot_code = db.Column(db.String(50), default="COT-0000")
+    require_imf = db.Column(db.Boolean, default=True)
+    require_cot = db.Column(db.Boolean, default=True)
 
-
-    admin_name = db.Column(
-        db.String(100),
-        default="Administrator"
-    )
-
-
-    email = db.Column(
-        db.String(120),
-        default="admin@novabank.com"
-    )
-
-
-    bank_name = db.Column(
-        db.String(100),
-        default="NovaBank"
-    )
-
-
-    currency = db.Column(
-        db.String(10),
-        default="USD"
-    )
-
-
-    created_at = db.Column(
-        db.DateTime,
-        default=datetime.utcnow
-    )
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
